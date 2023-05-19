4f205ce757a8e0c5dc52e793e4a230adf0c8371a
bombies <xjamesdzn@gmail.com>
2023-05-19 12:33:09 -0400

    Add greens-pub project

--- src/app/(projects)/ProjectLayout.tsx
+++ src/app/(projects)/ProjectLayout.tsx
@@ -38,7 +38,7 @@ export default function ProjectLayout(props: Props) {
     return (
         <div>
             <div
-                className='pt-32 h-[30rem] align-middle bg-gradient-conic from-neutral-950 to-red-950'
+                className='py-32 h-[40rem] align-middle bg-gradient-conic from-neutral-950 to-red-950'
             >
                 <div className='pl-24 phone:pl-6'>
                     <div className='h-fit w-fit mb-12'>
@@ -59,7 +59,7 @@ export default function ProjectLayout(props: Props) {
                                 router.push(props.website)
                             }}/>
                     </div>
-                    <h1 className={`text-5xl phone:text-lg text-left ${sfMono.variable} font-mono mb-3 mt-3`}>{props.title}</h1>
+                    <h1 className={`text-5xl max-w-4xl phone:max-w-lg phone:text-lg text-left ${sfMono.variable} font-mono mb-3 mt-3`}>{props.title}</h1>
                     <p className={`text-lg phone:text-sm text-left ${sfMono.variable} font-mono max-w-xl phone:max-w-xs mb-6`}>{props.subTitle}</p>
                     <Button type={ButtonType.CTA} label='visit' href={props.website} icon={linkIcon} width={6} height={3} />
                 </div>
---
+++ src/app/(projects)/greens-pub/page.tsx
@@ -0,0 +1,47 @@
+import ProjectLayout from "@/app/(projects)/ProjectLayout";
+import ContentSection from "@/components/ContentSection";
+import List from "@/components/List";
+
+export default function GreensPub() {
+    return (
+        <main>
+            <ProjectLayout
+                website='https://github.com/bombies/greens-restaurant-website'
+                icon='https://i.imgur.com/HLTQ78m.png'
+                title="green's restaurant & pub management website (in development)"
+                subTitle="A dashboard used to handle the managing of Green's Restaurant & Pub"
+                images={[
+                    'https://i.imgur.com/fuTnktJ.png',
+                    'https://i.imgur.com/mdXDU0u.png',
+                    'https://i.imgur.com/c1NiQOF.png',
+                    'https://i.imgur.com/x5K6xQJ.png',
+                    'https://i.imgur.com/RaaFYH0.gif'
+                ]}
+            >
+                <ContentSection
+                    title='about'
+                    description="This website was aims to manage the day-to-day stock, employee and invoice operations at a small business known as Green's Restaurant & Pub. It's made entirely in Next.js. So far the website only has functionality to manage stock and add and remove employees to and from the dashboard.<br/><br/>
+                    Further development on this project will introduce invoices that can be generated from the dashboard and employee management with mailing lists. It already has certain features implemented such as custom permissions which uses binary arithmetic and secure password hashing and storage.<br/><br/>
+                    This project was the first I made using Next.js, and it certainly was an essential foundational piece."
+                >
+                    <h3 className='text-3xl my-6 font-bold text-secondary'>tech stack</h3>
+                    <div className='flex space-x-8'>
+                        <List
+                            heading='back-end technologies'
+                            items={[
+                                'MongoDB',
+                                'Next.js API Routes',
+                            ]}
+                        />
+                        <List
+                            heading='front-end technologies'
+                            items={[
+                                'Next.js (TypeScript)',
+                            ]}
+                        />
+                    </div>
+                </ContentSection>
+            </ProjectLayout>
+        </main>
+    )
+}
