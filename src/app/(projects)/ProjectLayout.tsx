ea9d76b3d157a257ea5835e79513e92987876b9e
bombies <xjamesdzn@gmail.com>
2023-11-06 22:04:00 -0500

    Update UI

--- src/app/(projects)/ProjectLayout.tsx
+++ src/app/(projects)/ProjectLayout.tsx
@@ -26,61 +26,59 @@ export default function ProjectLayout(props: Props) {
     return (
         <div>
             <div
-                className='min-h-[90vh] flex justify-center items-center'
+                className='min-h-[90vh] w-full flex justify-center items-center'
                 style={{
                     backgroundImage: 'url(static/blur.png)'
                 }}
             >
                 <Card
                     classNames={{
-                        base: "w-fit dark:bg-neutral-900/50 bg-neutral-300/40 p-12 backdrop-blur-md",
+                        base: " dark:bg-neutral-900/50 bg-neutral-300/40 p-12 phone:px-6 backdrop-blur-md",
                     }}
                 >
                     <CardBody>
-                        <div>
-                            <div className='h-fit w-fit mb-12'>
-                                <Link href='/'>
-                                    <div
-                                        className='bg-neutral-100 text-primary dark:bg-black/20 hover:dark:bg-black/70 backdrop-blur-md p-4 border-1 border-primary/70 dark:border-white/20 rounded-2xl flex gap-4 my-auto hover:scale-[103%] transition-fast'>
-                                        <BackIcon/>
-                                        <p className='text-lg relative self-center'>Go Home</p>
-                                    </div>
-                                </Link>
-                            </div>
-                            <div className="flex phone:flex-col gap-4">
-                                <div className='transition-fast hover:scale-[101%]'>
-                                    <IconButton
-                                        icon={props.icon || projectIcon}
-                                        size={5}
-                                        className='animate-bounce-slow'
-                                        onClick={() => {
-                                            router.push(props.website)
-                                        }}/>
+                        <div className='h-fit w-fit mb-12'>
+                            <Link href='/'>
+                                <div
+                                    className='bg-neutral-100 text-primary dark:bg-black/20 hover:dark:bg-black/70 backdrop-blur-md p-4 border-1 border-primary/70 dark:border-white/20 rounded-2xl flex gap-4 my-auto hover:scale-[103%] transition-fast'>
+                                    <BackIcon/>
+                                    <p className='text-lg relative self-center'>Go Home</p>
                                 </div>
-                                <div>
-                                    <h1 className={`text-6xl  max-w-lg phone:text-3xl text-left font-black mb-3 tracking-wide capitalize text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-rose-600 drop-shadow-md`}>{props.title}</h1>
-                                    <p className={`text-xl phone:text-lg text-left max-w-xl tracking-wide phone:max-w-xs mb-6 text-white`}>{props.subTitle}</p>
-                                    <div className='flex phone:flex-col gap-4'>
+                            </Link>
+                        </div>
+                        <div className="flex phone:flex-col gap-4">
+                            <div className='transition-fast hover:scale-[101%]'>
+                                <IconButton
+                                    icon={props.icon || projectIcon}
+                                    size={5}
+                                    className='animate-bounce-slow'
+                                    onClick={() => {
+                                        router.push(props.website)
+                                    }}/>
+                            </div>
+                            <div>
+                                <h1 className={`text-5xl  max-w-lg phone:text-3xl text-left font-black mb-3 tracking-wide capitalize text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-rose-600 drop-shadow-md`}>{props.title}</h1>
+                                <p className={`text-xl phone:text-lg text-left max-w-xl tracking-wide phone:max-w-xs mb-6 text-white`}>{props.subTitle}</p>
+                                <div className='flex phone:flex-col gap-4'>
+                                    <Button
+                                        size="lg"
+                                        as={Link}
+                                        isExternal
+                                        showAnchorIcon
+                                        color="primary"
+                                        href={props.website}
+                                    >Visit</Button>
+                                    {props.secondaryWebsite &&
                                         <Button
                                             size="lg"
                                             as={Link}
                                             isExternal
-                                            showAnchorIcon
-                                            color="primary"
-                                            href={props.website}
-                                        >Visit</Button>
-                                        {props.secondaryWebsite &&
-                                            <Button
-                                                size="lg"
-                                                as={Link}
-                                                isExternal
-                                                color="secondary"
-                                                href={props.secondaryWebsite}
-                                            >
-                                                Also Visit
-                                            </Button>
-                                        }
-                                    </div>
+                                            color="secondary"
+                                            href={props.secondaryWebsite}
+                                        >
+                                            Also Visit
+                                        </Button>
+                                    }
                                 </div>
                             </div>
                         </div>
